bf41b08f0da4b935a602fd004bc7aa5532419518
tellmant <petukhov.m05@gmail.com>
2023-08-11 15:50:05 +0300

    Added Enemies enum.

---
+++ com/softserve/edu/finalproject/enemy/Enemies.java
@@ -0,0 +1,26 @@
+package com.softserve.edu.finalproject.enemy;
+
+public enum Enemies {
+    GOBLIN("Goblin"),
+    SKELETON("Skeleton"),
+    DRAGON("Dragon"),
+    HUMAN("Human"),
+    ELF("Elf"),
+    DWARF("Dwarf"),
+    ORK("Ork"),
+    UNDEAD("Undead");
+
+    private String name;
+
+    Enemies(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
